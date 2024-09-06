d9d45ed8876e0822f0f3322b18432fe72dbbde88
Jacob Hreshchyshyn <jakeyhresh123@gmail.com>
2024-09-06 13:28:29 -0700

    Printing Hello Test2!

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,7 +7,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Test1!");
+        Debug.Log("Hello Test2!");
     }
 
     // Update is called once per frame
